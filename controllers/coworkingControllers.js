bab2e69db96a33048158a5f84cc421b51301d3c4
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-07-18 17:47:47 +0200

    reorganise route & create controllers

---
+++ controllers/coworkingControllers.js
@@ -0,0 +1,114 @@
+// Coworking Controllers
+const mockCoworkings = require('../db/mock-coworkings');
+
+exports.findAllCoworkings = (req, res) => {
+    const criterium = req.query.criterium ? req.query.criterium : 'superficy'
+    const orderBy = req.query.orderBy || 'ASC'
+    const arrToSort = [...mockCoworkings];
+    const nosort = req.query.nosort
+
+    if (!nosort && (orderBy === 'ASC' || orderBy === 'DESC') 
+    && (criterium === 'superficy' || criterium === 'capacity')) 
+    {
+        arrToSort.sort((a, b) => {
+            return orderBy === 'DESC' ? 
+            b[criterium] - a[criterium] : a[criterium] - b[criterium]
+        })
+    }
+    res.json({ message: 
+        'La liste des coworkings a bien été récupérée.',
+         data: arrToSort
+        })
+};
+
+exports.findCoworkingByPk = (res, req)=>{
+    /*Old Ver
+//console.log(parseInt(req.params.id), mockCoworkings[0].name);
+        // :request-element => req.params.resquest-element
+        // let targetCoworking;
+        // for (let i = 0; i < mockCoworkings.length; i++)
+        // {
+        //     const element = mockCoworkings[i];
+        //     // :id => req.params.id requette req  toujours champ texte -> convert n b with parseInt
+        //     if (element.id === parseInt(req.params.id)){
+        //         targetCoworking = element;
+        //         break;
+        //     }
+        // }
+    //console.log(req.params.name);
+        // return
+        // res.send(`welcome to homepage, voici la requette: ${targetCoworking.name}`); // return text
+        //console.log("Request", req);
+        
+        //res.send(`Nom du coworking: ${targetCoworking ? targetCoworking.name :  'inconnu'}`);
+*/
+    // Init Id
+    let targetCoworking = mockCoworkings.find(el => el.id === parseInt(req.params.id)) ;
+    
+    targetCoworking ? (res.json({  
+        Message: `Info du coworking N°${targetCoworking.id} récupéré`, data:targetCoworking
+    })) : (
+        res.json({message:
+            `Le coworking N°${req.params.id} n'existe pas.`
+    }));
+};
+
+exports.createCoworking = (req, res) =>{
+    // Create New Item with increment id
+    // on ajoute à un nouvel objet {} un id unique, en l'occurrence égal au dernier id du mock-coworkings auquel on ajoute 1
+    const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
+    const newCoworking = { id: newId, ...req.body }
+    // Insert Data object Info
+    mockCoworkings.push(newCoworking)
+    return res.json({ message: 
+        `Un nouveau coworking n°${newCoworking.id} a été créé.`,
+        data: newCoworking 
+    });
+}
+
+exports.updateCoworking = (req, res) =>{
+    // Init Object Id
+    const indexIntArray = mockCoworkings.findIndex((element) =>{
+        return  element.id === parseInt(req.params.id)
+    })
+    // storage Updated Data
+    let = updatedCoworking = {...mockCoworkings[indexIntArray], ...req.body};
+    mockCoworkings[indexIntArray] = updatedCoworking;
+    // Check Id & Update
+    if(indexIntArray !== -1){
+        return res.json({message: 
+            `le coworking N°${updatedCoworking.id} : ${updatedCoworking.name} a été modifié`,
+            data:updatedCoworking
+        });
+    } else{
+        return res.json({message: 
+            `le coworking N°${req.params.id} n'existe pas.`
+        });
+    }
+};
+
+exports.deleteCoworking = (res, req) =>{
+    // Check Id & Storage Old Object Info
+    const indexIntArray = mockCoworkings.findIndex((element) =>{
+        return  element.id === parseInt(req.params.id)
+    })
+
+    /*Old
+    // if (mockCoworkings[indexIntArray].id !== null){
+        // delete mockCoworkings[indexIntArray]
+    // }  else{
+        // return res.json({
+            // // message: `Le coworking N°${deletedCoworkings.id} n'existe pas ou a déjà été supprimé`});
+    // }    */
+
+    // Check & Delete Queries demand
+    if(indexIntArray === -1){
+        return res.json({
+            message: `Le coworking N°${req.params.id} n'existe pas ou a déjà été supprimé`});
+    } else {
+        let deletedCoworkings = {...mockCoworkings[indexIntArray], ...req.body};
+        mockCoworkings.splice(indexIntArray, 1);
+        return  res.json(
+            {message: `Le coworking N°${deletedCoworkings.id}: ${deletedCoworkings.name} a été suprimé`, data:deletedCoworkings});
+    }
+};
--- routes/coworkingRoutes.js
+++ routes/coworkingRoutes.js
@@ -1,104 +1,19 @@
 // Routes Coworking Files
 const express = require('express')
 const router = express.Router()
-
-//Library
-const mockCoworkings = require('../db/mock-coworkings');
+const coworkingController = require('../controllers/coworkingControllers');
 
 // Queries
 router
     .route('/')
-    .get((req, res) => {
-        const criterium = req.query.criterium ? req.query.criterium : 'superficy'
-        const orderBy = req.query.orderBy || 'ASC'
-        const arrToSort = [...mockCoworkings];
-        const nosort = req.query.nosort
-
-        if (!nosort && (orderBy === 'ASC' || orderBy === 'DESC') && (criterium === 'superficy' || criterium === 'capacity')) {
-
-            arrToSort.sort((a, b) => {
-                return orderBy === 'DESC' ? b[criterium] - a[criterium] : a[criterium] - b[criterium]
-            })
-        }
-        res.json({ message: 'La liste des coworkings a bien été récupérée.', data: arrToSort })
-    })
-    .post((req, res) => {
-        // on ajoute à un nouvel objet {} un id unique, en l'occurrence égal au dernier id du mock-coworkings auquel on ajoute 1
-        const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
-        const newCoworking = { id: newId, ...req.body }
-        mockCoworkings.push(newCoworking)
-        return res.json({ message: `Un nouveau coworking n°${newCoworking.id} a été créé.`, data: newCoworking })
-    })
+    .get(coworkingController.findAllCoworkings)
+    .post(coworkingController.createCoworking)
 
 router
     .route('/:id')
-    .get((req, res)=>{
-    
-        //console.log(parseInt(req.params.id), mockCoworkings[0].name);
-        // :request-element => req.params.resquest-element
-        // let targetCoworking;
-        // for (let i = 0; i < mockCoworkings.length; i++)
-        // {
-        //     const element = mockCoworkings[i];
-        //     // :id => req.params.id requette req  toujours champ texte -> convert n b with parseInt
-        //     if (element.id === parseInt(req.params.id)){
-        //         targetCoworking = element;
-        //         break;
-        //     }
-        // }
-        let targetCoworking = mockCoworkings.find(el => el.id === parseInt(req.params.id)) ;
-    
-        //console.log(req.params.name);
-        // return
-        // res.send(`welcome to homepage, voici la requette: ${targetCoworking.name}`); // return text
-        //console.log("Request", req);
-        
-        //res.send(`Nom du coworking: ${targetCoworking ? targetCoworking.name :  'inconnu'}`);
-        
-        targetCoworking ? (res.json({  
-            Message: `Info du coworking N°${targetCoworking.id} récupéré`, data:targetCoworking
-        })) : (
-            res.json({message:`Le coworking N°${req.params.id} n'existe pas.`
-        }));
-    
-    })
-    .put((req, res) =>{
-        const indexIntArray = mockCoworkings.findIndex((element) =>{
-            return  element.id === parseInt(req.params.id)
-        })
-        let = updatedCoworking = {...mockCoworkings[indexIntArray], ...req.body};
-    
-        mockCoworkings[indexIntArray] = updatedCoworking;
-    
-        if(indexIntArray !== -1){
-            return res.json({message: `le coworking N°${updatedCoworking.id} : ${updatedCoworking.name} a été modifié`, data:updatedCoworking});
-        } else{
-            return res.json({message: `le coworking N°${req.params.id} n'existe pas.`});
-        }
-        
-    })
-    .delete((req, res) =>{
-        const indexIntArray = mockCoworkings.findIndex((element) =>{
-            return  element.id === parseInt(req.params.id)
-        })
-        let coworkingDeleted = {...mockCoworkings[indexIntArray], ...req.body};
-    
-        /*
-        if (mockCoworkings[indexIntArray].id !== null){
-            delete mockCoworkings[indexIntArray]
-        }  else{
-            return res.json({
-                message: `Le coworking N°${coworkingDeleted.id} n'existe pas ou a déjà été supprimé`});
-        }*/
-        if(indexIntArray === -1){
-            return res.json({
-                message: `Le coworking N°${req.params.id} n'existe pas ou a déjà été supprimé`});
-        } else {
-            mockCoworkings.splice(indexIntArray, 1);
-            return  res.json(
-                {message: `Le coworking N°${coworkingDeleted.id}: ${coworkingDeleted.name} a été suprimé`, data:coworkingDeleted});
-        }
-    })
+    .get(coworkingController.findCoworkingByPk)
+    .put(coworkingController.updateCoworking)
+    .delete(coworkingController.deleteCoworking)
 
 // Export
 module.exports = router;
